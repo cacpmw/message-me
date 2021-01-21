f18099aefd08818178572c6069c7e729fd8af369
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2021-01-21 13:52:28 -0300

    user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Carlos', password: '123456')
+User.create(username: 'Vinicius', password: '123456')
+User.create(username: 'Pedro', password: '123456')
+User.create(username: 'Marcos', password: '123456')
+User.create(username: 'Ricardo', password: '123456')
